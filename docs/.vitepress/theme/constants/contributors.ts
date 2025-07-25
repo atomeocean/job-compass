eee293302aa41618a9fe2bdadd2cb95b4b038c1d
Bojian Wang <90784040+bojianwangny@users.noreply.github.com>
2025-07-25 14:56:52 -0400

    更新贡献者信息 (#461)
    * update contributor
    
    * remove import

--- docs/.vitepress/theme/constants/contributors.ts
+++ docs/.vitepress/theme/constants/contributors.ts
@@ -2,8 +2,21 @@
  * name：在页面中展示的作者名称
  * username：相关联的github用户名
  * mapByNameAliases：使用的git名称列表，可对列表中的名称一一匹配
+ * avatar：头像链接
+ * links：profile跳转链接（可添加多个跳转链接，但贡献者列表只会默认选择第一个链接进行跳转）
  */
-export const mapAuthors = [
+export const contributors = [
+  // Ai助手
+  {
+    name: 'Copilot',
+    username: 'Copilot',
+    avatar: 'https://atomeocean.cn/contributor-avatar/default-contributor-avatar.webp',
+    links: [
+      { type: '', icon: 'github', link: 'https://github.com/atomeocean' }
+    ]
+  },
+
+  // 贡献者名单
   {
     name: 'Bojian',
     username: 'bojianwangny',
@@ -17,5 +30,17 @@ export const mapAuthors = [
   {
     name: 'Bella Zhong',
     username: 'BellaZ0317',
-  }
+  },
+  {
+    name: 'Colton',
+    username: 'tengtianxiang',
+  },
+  {
+    name: 'Han',
+    username: 'MHeartSutra',
+  },
+  {
+    name: 'Mark',
+    username: 'nepalter',
+  },
 ]
